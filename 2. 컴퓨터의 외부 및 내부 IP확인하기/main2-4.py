c541e5b43e1bd598824aa977238f1e1298a17921
정인성 <sosist12@gmail.com>
2022-11-30 18:27:41 +0900

    Update main2-4.py
    설명 주석 달기

--- 2. 컴퓨터의 외부 및 내부 IP확인하기/main2-4.py
+++ 2. 컴퓨터의 외부 및 내부 IP확인하기/main2-4.py
@@ -1,6 +1,6 @@
 import requests
 import re
-req = requests.get("http://ipconfig.kr")
+req = requests.get("http://ipconfig.kr")  #http://ipconfig.kr 사이트에서 가져오기
 
-out_addr = re.search(r'IP Address : (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', req.text)[1]
+out_addr = re.search(r'IP Address : (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', req.text)[1] #'페이지 내에서 해당하는 부분을 찾고 텍스트로 받아오기
 print(out_addr)
