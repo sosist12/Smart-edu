3044732a0c7ae38878d8d91f4304b39a0159f081
sosist12 <sosist12@gmail.com>
2022-12-01 19:36:13 +0900

    python 9일차

--- 37. 인공지능 사과와 오렌지 구분하기/main37-1.py
+++ 37. 인공지능 사과와 오렌지 구분하기/main37-1.py
@@ -8,10 +8,10 @@ image_path = r"37. 인공지능 사과와 오렌지 구분하기\검증용사진
 
 model = tensorflow.keras.models.load_model(model_path)
 
-data = np.ndarray(shape=(1, 244, 244, 3), dtype=np.float32)
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 image = Image.open(image_path)
-size = (244,244)
+size = (224,224)
 image = ImageOps.fit(image, size, Image.ANTIALIAS)
 
 image_array = np.asarray(image)
